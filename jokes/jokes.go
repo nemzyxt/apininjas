cfbe25abd6fcdbc27c0c03f186d24a941fb4d633
Nemuel Wainaina <nemuelwainaina@proton.me>
2023-12-12 11:56:20 +0300

    Add client for the Jokes API

---
+++ jokes/jokes.go
@@ -0,0 +1,42 @@
+package jokes
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nemzyxt/apininjas/utils"
+)
+
+const (
+	endpoint = utils.BaseUrl + "jokes?limit="
+)
+
+type JokesClient struct {
+	utils.Client
+}
+
+type Joke struct {
+	Joke string
+}
+
+func NewClient(apiKey string) JokesClient {
+	return JokesClient{
+		Client: utils.Init(apiKey),
+	}
+}
+
+func (c *JokesClient) GetJokes(limit int) ([]Joke, error) {
+	url := endpoint + fmt.Sprint(limit)
+
+	resp, err := utils.MakeRequest(url, c.ApiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	var jokes []Joke
+	if err := json.NewDecoder(resp.Body).Decode(&jokes); err != nil {
+		return nil, fmt.Errorf("error decoding api response: %v", err)
+	}
+
+	return jokes, nil
+}
